429fc8511c77b987cef2dfade6668020ed581e3f
Bahul Neel Upadhyaya <bahul@talktoloop.org>
2024-12-12 23:27:41 +0000

    refactor(ui-atomic): streamline Menu Bar component bindings for improved clarity
    - Updated the Item.vue component to simplify attribute bindings by removing unnecessary spread operators in the VariantAssembly and its templates.
    - Enhanced the readability of the component by directly binding attributes, aligning with previous refactoring efforts to improve maintainability and consistency across Menubar components.
    
    This change contributes to a cleaner and more efficient code structure, supporting ongoing improvements in the UI atomic design system.

--- layers/ui-atomic/components/Molecule/Menu/Bar/Item.vue
+++ layers/ui-atomic/components/Molecule/Menu/Bar/Item.vue
@@ -24,22 +24,22 @@ const variants = {
 </script>
 
 <template>
-  <VariantAssembly :value="item" :variants="variants">
+  <VariantAssembly :value="item" :variants="variants" v-bind="{ ...options }">
     <template #separator="{ attrs }">
-      <shadcn-menubar-separator v-bind="{ ...options, ...attrs }" />
+      <shadcn-menubar-separator v-bind="attrs" />
     </template>
     <template #checkbox="{ attrs, variant: { value } }">
-      <shadcn-menubar-checkbox-item :checked="value.checked" v-bind="{ ...options, ...attrs }" />
+      <shadcn-menubar-checkbox-item :checked="value.checked" v-bind="attrs" />
     </template>
     <template #radioGroup="{ attrs, variant: { value } }">
-      <MoleculeMenuBarRadioGroup :item="value" v-bind="{ ...options, ...attrs }" />
+      <MoleculeMenuBarRadioGroup :item="value" v-bind="attrs" />
     </template>
     <template #menu="{ attrs, variant: { value } }">
-      <MoleculeMenuBarMenu v-if="root" :item="value" v-bind="{ ...options, ...attrs }" />
-      <MoleculeMenuBarSub v-else :item="value" v-bind="{ ...options, ...attrs }" />
+      <MoleculeMenuBarMenu v-if="root" :item="value" v-bind="attrs" />
+      <MoleculeMenuBarSub v-else :item="value" v-bind="attrs" />
     </template>
     <template #item="{ attrs, variant: { value } }">
-      <shadcn-menubar-item v-bind="{ ...options, ...attrs }">
+      <shadcn-menubar-item v-bind="attrs">
         {{ value.label }}
         <shadcn-menubar-shortcut v-if="value.shortcut">
           {{ value.shortcut }}
@@ -47,7 +47,7 @@ const variants = {
       </shadcn-menubar-item>
     </template>
     <template #default="{ attrs, variant: { value } }">
-      <shadcn-menubar-item v-bind="{ ...options, ...attrs }">
+      <shadcn-menubar-item v-bind="attrs">
         Unknown item: {{ JSON.stringify(value) }}
       </shadcn-menubar-item>
     </template>
