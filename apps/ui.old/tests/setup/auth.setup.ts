bfb866966279dc7a7d7f12432fcd2a0490416901
Abe Shalash <shalashtein@gmail.com>
2025-06-30 00:10:18 +0200

    garbage collection

--- apps/ui.old/tests/setup/auth.setup.ts
+++
@@ -1,9 +0,0 @@
-import { test as setup } from '@_playwright-src/fixtures/merge.fixture';
-
-setup('authenticate as admin @admin', async ({ loginAdmin }) => {
-  loginAdmin;
-});
-
-setup('authenticate as user', async ({ loginUser }) => {
-  loginUser;
-});
